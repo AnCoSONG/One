94328d6ac2706ad7852df45dc2a63188182ab012
AnCoSONG <1197252996@qq.com>
2021-02-19 04:53:38 +0800

    use remote database

--- src/nest/src/app.module.ts
+++ src/nest/src/app.module.ts
@@ -2,16 +2,20 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'
 import { ArticleModule } from './article/article.module';
 
 @Module({
   imports: [
     ArticleModule,
-    MongooseModule.forRoot('mongodb://admin:admin2021@localhost/oneblog', {
+    MongooseModule.forRoot('mongodb://admin:admin2021@one.anco.fun/oneblog', {
       useNewUrlParser: true,
       useFindAndModify: false,
       useCreateIndex: true,
       useUnifiedTopology: true,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
   ],
   controllers: [AppController],
