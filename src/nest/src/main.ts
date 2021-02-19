2b7fb7c597366b947192eb934ef0a545b41cc382
AnCoSONG <1197252996@qq.com>
2021-02-19 16:48:39 +0800

    new deploy strategy: api.anco.fun for backend

--- src/nest/src/main.ts
+++ src/nest/src/main.ts
@@ -4,16 +4,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.setGlobalPrefix('api');
+  // 后端接口统一放在api.anco.fun下，取消api前缀
+  // app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
     .setTitle('Nest Learner Demo')
     .setDescription('API description')
     .setVersion('1.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config, {
-    ignoreGlobalPrefix: true,
-  });
-  SwaggerModule.setup('api/api-doc', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
   await app.listen(5000);
 }
 bootstrap();
